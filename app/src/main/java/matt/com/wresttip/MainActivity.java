d00f51dcb6a43b8d940bc7fa5f1d907926967abd
Matthew Duane <duanemat@gmail.com>
2015-07-04 21:10:03 -0400

    Initial commit.

---
+++ app/src/main/java/matt/com/wresttip/MainActivity.java
@@ -0,0 +1,57 @@
+package matt.com.wresttip;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements Button.OnClickListener {
+
+    private TextView mTextView;
+    private WatchViewStub stub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        /*Button btn = new Button(this.getApplicationContext());
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) btn.getLayoutParams();
+        if (lp == null){
+            lp = new LinearLayout.LayoutParams(25, 25);
+        }else {
+            lp.width = 25;
+            lp.height = 25;
+        }
+        lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL;
+        btn.setLayoutParams(lp);
+        btn.setBackgroundResource(R.drawable.round_button);
+        btn.setText("2");
+        btn.setTextColor(getResources().getColor(android.R.color.white));
+
+        LinearLayout ll = (LinearLayout) this.findViewById(R.id.wear_layout);
+        ll.addView(btn);*/
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button btn = (Button)view;
+        Toast.makeText(this.getApplicationContext(), btn.getText(), Toast.LENGTH_SHORT).show();
+    }
+}
